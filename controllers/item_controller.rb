c88315e44b357ec6efebe6ec02845796d26f6d68
Daniel Stewart <mgdog1@googlemail.com>
2018-05-07 15:32:07 +0100

    added add item page

--- controllers/item_controller.rb
+++ controllers/item_controller.rb
@@ -1,11 +1,12 @@
 require 'sinatra'
 require 'sinatra/contrib/all' if development?
 require_relative '../models/item'
-
-
-
+require_relative '../models/school'
+require_relative '../models/supplier'
 
 get '/add-item' do
+  @schools = School.read_all.sort_by!{|school| school.name}
+  @suppliers = Supplier.read_all.sort_by!{|supplier| supplier.name}
   erb :"items/add_item"
 end
 
--- tesm_controller.rb
+++ tesm_controller.rb
@@ -1,8 +1,11 @@
 require 'sinatra'
 require 'sinatra/contrib/all' if development?
 require_relative 'models/inventory'
+require_relative 'controllers/item_controller'
+require_relative 'controllers/school_controller'
+require_relative 'controllers/supplier_controller'
 
 get '/' do
   @inventory = Inventory.read_all
-  erb :inventory_index
+  erb :"items/inventory_index"
 end
