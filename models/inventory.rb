7fee59c8e20149054ba99a2bf1e097df35a4fd65
Daniel Stewart <mgdog1@googlemail.com>
2018-05-07 12:00:32 +0100

    initial implementation of inventory methods

--- models/inventory.rb
+++ models/inventory.rb
@@ -16,7 +16,38 @@ class Inventory
     @id = result[0]["id"].to_i
   end
 
-  
+  def update
+    sql = "UPDATE inventory SET(item_id, quantity) = ($1, $2)
+    WHERE id = $3"
+    SqlRunner.run sql, [@item_id, @quantity, @id]
+  end
+
+  def delete
+    sql = "DELETE FROM inventory WHERE id = $1"
+    SqlRunner.run sql, [@id]
+  end
+
+  def increase_qty amount
+    return unless amount >= 0
+    @quantity += amount
+  end
+
+  def decrease_qty amount
+    return unless amount >= 0
+    @quantity -= amount
+  end
+
+  def self.read_by_item item
+    sql = "SELECT * FROM inventory WHERE item_id = $1"
+    result = SqlRunner.run sql, [item.id]
+    build_results(result).first
+  end
+
+  def self.read_all
+    sql = "SELECT * FROM inventory"
+    results = SqlRunner.run sql
+    build_results results
+  end
 
   def self.delete_all
     sql = "DELETE FROM inventory"
