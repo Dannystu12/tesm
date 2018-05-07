daac8fabe7ec1309ef32ba1ab2d52d4e0ffbfb86
Daniel Stewart <mgdog1@googlemail.com>
2018-05-07 14:27:36 +0100

    added ability to create suppliers and supplier index

--- tesm_controller.rb
+++ tesm_controller.rb
@@ -7,5 +7,19 @@ require_relative 'models/school'
 
 get '/' do
   @inventory = Inventory.read_all
-  erb :index
+  erb :inventory_index
+end
+
+get '/suppliers' do
+  @suppliers = Supplier.read_all
+  erb :supplier_index
+end
+
+get '/add-supplier' do
+  erb :add_supplier
+end
+
+post '/add-supplier' do
+  Supplier.new(params).create
+  redirect "/suppliers"
 end
